e1b9aa5450cdf48ea7df5c1941f89fb442b84d03
Farzana Jahan <32354089+Farzana-Jahan@users.noreply.github.com>
2020-10-30 11:15:21 +1000

    Update and rename BEL_sim_ind.sh to BEL_sim_BYM.sh

--- jobs/BEL_sim_BYM.sh
+++ jobs/BEL_sim_BYM.sh
@@ -3,10 +3,10 @@
 #PBS -N BEL-indSim
 #PBS -l ncpus=3
 #PBS -l mem=10GB
-#PBS -l walltime=24:00:00
+#PBS -l walltime=10:00:00
 #PBS -l cpuarch=avx2
-#PBS -o BEL-indSim_stdout.out
-#PBS -e BEL-indSim_stderr.out
+#PBS -o BEL-BYMSim_stdout.out
+#PBS -e BEL-BYMSim_stderr.out
 
 # More info on PBS directives can be found here
 # http://qcd.phys.cmu.edu/QCDcluster/pbs/run_serial.html
@@ -76,7 +76,7 @@ run_program(){
   #make sure we change to the current directory
   #where this bash job script is
   cd $PBS_O_WORKDIR
-  Rscript "Rscripts/sim_run_1_ind.R"
+  Rscript "Rscripts/sim_run_1.R"
   #this script installed all of the packages locally,
   #since you do not have root access to HPC.
   #This just means we need to let R now where we installed
